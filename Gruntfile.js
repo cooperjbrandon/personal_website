d9991f9c01823ca8149c6fd1d14c551ea768dadb
Brandon Cooper <cooperjbrandon@gmail.com>
2014-02-03 12:00:10 -0800

    concatted css file

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,21 +20,10 @@ module.exports = function(grunt) {
             }
         },
 
-        imagemin: {
-            dynamic: {
-                files: [{
-                    expand: true,
-                    cwd: 'images/',
-                    src: ['*.{png,jpg,gif}'],
-                    dest: 'images/build/'
-                }]
-            }
-        },
-
         cssmin: {
             build: {
-                src: 'js/build/application.js',
-                dest: 'js/build/application.min.js'
+                src: ['css/grid.css', 'css/normalize.css', 'css/style.css'],
+                dest: 'css/build/application.css'
             }
         },
 
@@ -44,10 +33,9 @@ module.exports = function(grunt) {
     // 3. Where we tell Grunt we plan to use this plug-in.
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-imagemin');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['concat', 'uglify', 'imagemin']);
+    grunt.registerTask('default', ['concat', 'uglify', 'cssmin']);
 
 };
